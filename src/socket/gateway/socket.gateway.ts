cc0b96eed4643acf1422c1f4b89bc1a091f5744d
mubarkjahbless <jahblessalalinga@gmail.com>
2024-06-20 22:33:36 +0000

    refactor: add port for socket

--- src/socket/gateway/socket.gateway.ts
+++ src/socket/gateway/socket.gateway.ts
@@ -19,7 +19,7 @@ import { MessageService } from '../../api/messages/service/messages.service';
 import { CreateDirectMessageDTO } from 'src/api/messages/dto/create-direct-message.dto';
 import { DirectMessagesService } from 'src/api/messages/service/direct-messages.service';
 
-@WebSocketGateway(parseInt(process.env.PORT, 10), {
+@WebSocketGateway(parseInt(process.env.SOCKET_PORT, 10), {
   cors: {
     methods: ['GET', 'POST'],
     allowedHeaders: 'Content-Type, x-requested-with',
